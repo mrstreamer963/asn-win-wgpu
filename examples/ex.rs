ee8081744efef82ba2200ff637b1ffac05be3ce6
mrstreamer963 <698lo7epy@mozmail.com>
2025-08-17 09:13:44 +0300

    move example to example

--- examples/ex.rs
+++ examples/ex.rs
@@ -1,6 +1,7 @@
 // https://gist.github.com/mrstreamer963/85ab238fd42b7a2077a22d179e1d1c54#file-main-rs-L82
 
 use std::sync::Arc;
+use std::time::Duration;
 
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
@@ -23,7 +24,13 @@ fn main() {
 
     let mut app = DemoApp::Loading;
 
-    event_loop.run_app(&mut app).unwrap()
+    event_loop.run_app(&mut app).unwrap();
+
+    println!("event_loop.run_app ended");
+
+    std::thread::sleep(Duration::from_secs(3));
+
+    println!("main ok");
 }
 
 impl ApplicationHandler for DemoApp {
