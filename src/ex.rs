6b6e64aca94d8acd8f47ee0c1432f927d0567f88
mrstreamer963 <698lo7epy@mozmail.com>
2025-08-17 09:10:57 +0300

    Even more simple example

---
+++ src/ex.rs
@@ -0,0 +1,64 @@
+// https://gist.github.com/mrstreamer963/85ab238fd42b7a2077a22d179e1d1c54#file-main-rs-L82
+
+use std::sync::Arc;
+
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::window::{Window, WindowAttributes, WindowId};
+
+#[derive(Debug)]
+struct DemoResource;
+
+enum DemoApp {
+    Loading,
+    Ready {
+        window: Arc<Window>,
+        resource: DemoResource,
+    },
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+
+    let mut app = DemoApp::Loading;
+
+    event_loop.run_app(&mut app).unwrap()
+}
+
+impl ApplicationHandler for DemoApp {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if let Self::Loading = self {
+            let window_attrs = WindowAttributes::default();
+
+            let window = event_loop.create_window(window_attrs).unwrap();
+
+            let resource = DemoResource;
+
+            *self = Self::Ready {
+                window: Arc::new(window),
+                resource,
+            }
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        let Self::Ready { window, resource } = self else {
+            return;
+        };
+
+        match event {
+            WindowEvent::RedrawRequested => {
+                println!("Window title: {}", window.title());
+                println!("Demo resource: {:?}", resource);
+            }
+            WindowEvent::CloseRequested => event_loop.exit(),
+            _ => {}
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,16 @@ use winit::{
     application::ApplicationHandler,
     event::WindowEvent,
     event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
-    window::{Window, WindowId},
+    window::{Window, WindowAttributes, WindowId},
 };
-
-use wgpu_utils::get_window;
 
 fn main() {
     let event_loop = EventLoop::new()
         .map_err(|e| format!("Failed to create event loop: {e}"))
         .unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
 
     let mut runner = Runner { s: None };
-    event_loop.set_control_flow(ControlFlow::Poll);
     event_loop.run_app(&mut runner).unwrap();
 
     println!("event_loop.run_app ended");
@@ -31,9 +29,6 @@ fn main() {
 
 pub struct State {
     pub window: Arc<Window>,
-    pub surface: wgpu::Surface<'static>,
-    pub device: wgpu::Device,
-    pub queue: wgpu::Queue,
 }
 
 struct Runner {
@@ -50,10 +45,13 @@ impl ApplicationHandler for Runner {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         println!("resumed!");
         if self.s.is_none() {
-            let w = Arc::new(get_window(event_loop));
+            let window_attrs = WindowAttributes::default();
+            let window = event_loop.create_window(window_attrs).unwrap();
+
+            let w = Arc::new(window);
             println!("Runner get_window ok");
 
-            let state = pollster::block_on(wgpu_utils::get_state(w.clone()));
+            let state = State { window: w.clone() };
             println!("Runner get_state ok");
 
             self.s = Some(Arc::new(Mutex::new(state)));
@@ -90,111 +88,5 @@ impl ApplicationHandler for Runner {
                 println!("window_event! {:?}", event);
             }
         }
-    }
-}
-
-mod wgpu_utils {
-    use std::sync::Arc;
-
-    use winit::{
-        event_loop::ActiveEventLoop,
-        window::{Window, WindowAttributes},
-    };
-
-    use crate::State;
-
-    pub async fn get_state(window: Arc<Window>) -> State {
-        let size = window.inner_size();
-        println!("window size: {size:?}");
-
-        let backend_features = wgpu::Instance::enabled_backend_features();
-        println!("backend_features: {backend_features:?}");
-
-        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
-            backends: wgpu::Backends::PRIMARY,
-            ..Default::default()
-        });
-
-        println!("instance ok");
-
-        let surface = instance.create_surface(window.clone()).unwrap();
-
-        println!("surface ok");
-
-        let adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::default(),
-                compatible_surface: Some(&surface),
-                force_fallback_adapter: false,
-            })
-            .await
-            .unwrap();
-
-        println!("adapter ok");
-
-        // Create device and queue
-        let (device, queue) = adapter
-            .request_device(&wgpu::DeviceDescriptor {
-                label: Some("ASN WGPU Device"),
-                required_features: wgpu::Features::empty(),
-                required_limits: if cfg!(target_arch = "wasm32") {
-                    wgpu::Limits::downlevel_webgl2_defaults()
-                } else {
-                    wgpu::Limits::default()
-                },
-                memory_hints: Default::default(),
-                trace: wgpu::Trace::Off,
-            })
-            .await
-            .unwrap();
-
-        println!("device, queue ok");
-
-        // Configure surface
-        let surface_caps = surface.get_capabilities(&adapter);
-
-        println!("surface_caps {:?}", surface_caps);
-
-        let surface_format = surface_caps
-            .formats
-            .iter()
-            .find(|f| f.is_srgb())
-            .copied()
-            .unwrap_or(surface_caps.formats[0]);
-
-        println!("surface_format {:?}", surface_format);
-
-        let config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: surface_format,
-            width: size.width,
-            height: size.height,
-            present_mode: surface_caps.present_modes[0],
-            alpha_mode: surface_caps.alpha_modes[0],
-            view_formats: vec![],
-            desired_maximum_frame_latency: 2,
-        };
-
-        println!("config ready");
-
-        surface.configure(&device, &config);
-
-        State {
-            surface,
-            device,
-            queue,
-            window,
-        }
-    }
-
-    pub fn get_window(event_loop: &ActiveEventLoop) -> Window {
-        let window_attributes = WindowAttributes::default()
-            .with_title("No title")
-            .with_inner_size(winit::dpi::LogicalSize::new(800, 600))
-            .with_resizable(true)
-            .with_decorations(true);
-
-        let w = event_loop.create_window(window_attributes).unwrap();
-        w
     }
 }
