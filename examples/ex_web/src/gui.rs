0dbbb2dfec0634f32fbfe2cfa7119bf116ae0c4f
mrstreamer963 <698lo7epy@mozmail.com>
2025-09-09 08:28:05 +0300

    update

--- examples/ex_web/src/gui.rs
+++ examples/ex_web/src/gui.rs
@@ -1,10 +1,6 @@
 use crate::gui_handler;
 use asn_wgpu::run_with_handler;
 
-#[allow(dead_code)]
-const LOG_MODULE_NAME: &str = "gui_wgpu";
-
-#[allow(dead_code)]
 pub fn run_gui() {
     let h = gui_handler::get_handler();
     run_with_handler(h);
