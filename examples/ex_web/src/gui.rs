31aeae2e52be7131f2b63dafcca43c648d79503c
mrstreamer963 <698lo7epy@mozmail.com>
2025-09-10 22:39:55 +0300

    some updates

--- examples/ex_web/src/gui.rs
+++ examples/ex_web/src/gui.rs
@@ -7,8 +7,13 @@ pub fn run_gui() {
     let map_height = 32;
     let map = get_map(map_width, map_height);
 
-    // прокидывать map --> GuiMap через очередь (?)
+    let map_tiles_bytes = include_bytes!("../../tiles_16_12.png");
+    let tiles_width = 16;
+    let tiles_height = 12;
+
+    // прокидывать map --> GuiMap через очередь (?) - как изначально в движке и планировалось
     // инициализировать GuiMap по дефолту пикселем 1x1 (?)
+    // model-view-controller (?) mvc через очередь! :3
 
     let h = gui_handler::get_handler();
 
