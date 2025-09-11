ceaec60842564e54dcc6877fc0270fe90ec76c02
mrstreamer963 <698lo7epy@mozmail.com>
2025-09-11 08:04:00 +0300

    add example to tokio-bus

---
+++ modules/tokio-bus/examples/ex_bus.rs
@@ -0,0 +1,26 @@
+use asn_core_bus::{AsnBus, AsnReceiver, AsnTransmitter};
+
+use tokio_bus::new_tokio_bus;
+
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+enum TaskType {
+    TaskNone,
+    TaskUpdate,
+}
+
+fn main() {
+    let bus = new_tokio_bus::<TaskType>(16);
+
+    let mut recv = bus.get_receiver();
+    let send = bus.get_sender();
+
+    send.send_message(TaskType::TaskUpdate).unwrap();
+    send.send_message(TaskType::TaskNone).unwrap();
+
+    let result1 = recv.get_message().unwrap();
+    let result2 = recv.get_message().unwrap();
+
+    println!("Result: {:?}", result1);
+    println!("Result: {:?}", result2);
+}
